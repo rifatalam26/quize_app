fccf64d45ee7f6d7a871a03e36ba14be69f2ed41
Md Rifat Alam <mdrftlm@gmail.com>
2025-02-26 07:04:11 +0600

    grit view.

--- lib/View/home_screen.dart
+++ lib/View/home_screen.dart
@@ -121,6 +121,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 SizedBox(
                   height: 60,
                 ),
+
                GridView.builder(
                  shrinkWrap: true,
                  itemCount: 55,
